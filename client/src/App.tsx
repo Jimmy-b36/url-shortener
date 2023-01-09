e101029e4edd7c402f6cd1cb83ff4caa5fd8a0f6
James Ball <jamie.j.ball@gmail.com>
2023-01-08 18:30:01 -0800

    fix: href link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,22 +4,28 @@ import shortenUrl from './helpers/urlShortener';
 
 function App() {
   const [shortUrl, setShortUrl] = useState<string>();
-  const url = useRef<string>();
+  const [url, setUrl] = useState<string>();
   const formRef = useRef();
   const handleFormSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
     setShortUrl(shortenUrl());
-    e.target.reset();
+    (e.target as HTMLFormElement).reset();
   };
   return (
     <div className="App">
       <form action="" ref={formRef.current} onSubmit={handleFormSubmit}>
         <label htmlFor="url-input">Please enter a url</label>
-        <input type="text" name="url-input" id="" ref={url.current} />
+        <input
+          type="text"
+          name="url-input"
+          id=""
+          value={url}
+          onChange={e => setUrl(e.target.value)}
+        />
         <input type="submit" value="Submit" />
       </form>
-      <a href={url.current} target="_blank" rel="noopener">
+      <a href={url} target="_blank" rel="noopener">
         {shortUrl}
       </a>
     </div>
