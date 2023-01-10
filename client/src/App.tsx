a760c77b9c45a44b82f91e2e8d2a91fc3a19af3c
James Ball <jamie.j.ball@gmail.com>
2023-01-10 15:51:03 -0800

    feat: urls & shortenedUrls are submitted to db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,56 @@
-import React, { useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 import shortenUrl from './helpers/urlShortener';
+import Url from './components/Url';
+
+export type UrlsArr = {
+  map(arg0: (url: UrlsArr) => JSX.Element): React.ReactNode;
+  _id: string;
+  url: string;
+  shortUrl: string;
+};
 
 function App() {
-  const [shortUrl, setShortUrl] = useState<string>();
-  const [url, setUrl] = useState<string>();
-  const formRef = useRef();
-  const handleFormSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const urlInput = useRef('');
 
-    setShortUrl(shortenUrl());
+  const [urls, setUrls] = useState<UrlsArr>();
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const shortUrl = await shortenUrl();
+    const url = urlInput.current;
+    await axios.post('http://localhost:3000/api/url', { url, shortUrl });
     (e.target as HTMLFormElement).reset();
   };
+
+  useEffect(() => {
+    const fetchUrls = async () => {
+      const { data }: { data: UrlsArr } = await axios.get(
+        'http://localhost:3000/api/url'
+      );
+      setUrls(data);
+    };
+    fetchUrls();
+  }, [urlInput.current, urls]);
+
   return (
     <div className="App">
-      <form action="" ref={formRef.current} onSubmit={handleFormSubmit}>
+      <form action="" onSubmit={handleFormSubmit}>
         <label htmlFor="url-input">Please enter a url</label>
         <input
           type="text"
           name="url-input"
           id=""
-          value={url}
-          onChange={e => setUrl(e.target.value)}
+          onChange={e => (urlInput.current = e.target.value)}
         />
         <input type="submit" value="Submit" />
       </form>
-      <a href={url} target="_blank" rel="noopener">
-        {shortUrl}
-      </a>
+      {urls &&
+        urls.map((url: UrlsArr) => (
+          <Fragment key={url._id}>
+            <Url url={url} setUrls={setUrls} />
+          </Fragment>
+        ))}
     </div>
   );
 }
