f3b192283bad5f2c9929fd08893c4561da127ea8
James Ball <jamie.j.ball@gmail.com>
2023-01-10 15:51:35 -0800

    feat: get urls & delete url

--- server/routes/urlRoutes.ts
+++ server/routes/urlRoutes.ts
@@ -1,13 +1,23 @@
 import express, { Request, Response } from 'express';
 const router = express.Router();
 const Url = require('../models/urlModel');
+import { Error } from 'mongoose';
 
 const urlRoutes = () => {
   router.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+    Url.find(
+      {},
+      (err: Error, urls: { _id: string; url: string; shortUrl: string }) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(urls);
+        }
+      }
+    );
   });
 
-  router.post('/create', async (req: Request, res: Response) => {
+  router.post('/', async (req: Request, res: Response) => {
     const newShortUrl = await new Url({
       url: req.body.url,
       shortUrl: req.body.shortUrl,
@@ -15,6 +25,14 @@ const urlRoutes = () => {
     try {
       await newShortUrl.save();
       res.status(201).json(newShortUrl);
+    } catch (err: any) {
+      res.status(400).json(err.message);
+    }
+  });
+
+  router.delete('/:id', async (req: Request, res: Response) => {
+    try {
+      const deletedUrl = await Url.findByIdAndDelete(req.params.id);
     } catch (err: any) {
       res.status(400).json(err.message);
     }
